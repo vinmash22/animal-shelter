43e2cbe898b05b2b5458378cc237dafcb03b3c09
vinmash <vinmash@mail.ru>
2023-09-19 20:33:20 +0500

    start

---
+++ src/main/java/pro/sky/java/course6/animalshelter/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,53 @@
+package pro.sky.java.course6.animalshelter.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            // Process your updates here
+            if (update.message() != null && update.message().text() != null) {
+                var text = update.message().text();
+                Long chatId = update.message().chat().id();
+                if ("/start".equals(text)) {
+                    telegramBot.execute(new SendMessage(chatId, "Добро пожаловать! " +
+                            "Выберите приют:" +
+                            " 1. Приют для собак. 2. Приют для кошек"));
+                }
+                            }
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
+
+
+
+
+
+
+
+
