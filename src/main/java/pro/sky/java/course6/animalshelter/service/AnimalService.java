0de81956a1512da2c133b70ec10189d671cc55c5
vinmash <vinmash@mail.ru>
2023-10-21 23:04:01 +0500

    sheduller

--- src/main/java/pro/sky/java/course6/animalshelter/service/AnimalService.java
+++ src/main/java/pro/sky/java/course6/animalshelter/service/AnimalService.java
@@ -5,6 +5,8 @@ import pro.sky.java.course6.animalshelter.entity.User;
 import pro.sky.java.course6.animalshelter.repository.AnimalRepository;
 import pro.sky.java.course6.animalshelter.repository.UserRepository;
 
+import java.util.Date;
+
 @Service
 public class AnimalService {
 
@@ -29,8 +31,12 @@ public class AnimalService {
         Animal animal = animalRepository.findAnimalById(idAnimal);
         User user = userRepository.findUserById(idUser);
         animal.setUser(user);
+        Date date = new Date();
+        animal.setDate(date);
         return animalRepository.save(animal);
     }
+
+
 }
 
 
